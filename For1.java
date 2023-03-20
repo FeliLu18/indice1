d9a2216fc3e34b6b3a261f952fb884d749122271
FeliLu18 <127162822+FeliLu18@users.noreply.github.com>
2023-03-19 21:04:51 -0300

    Add files via upload

---
+++ For1.java
@@ -0,0 +1,14 @@
+package com.rdl;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Numeros del 1 al 100 (Usando For): ");
+		int i;
+		for(i=1;i<=100;i++)
+			System.out.println(i*2);
+
+	}
+
+}
